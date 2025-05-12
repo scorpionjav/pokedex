1523f89d39408060aceb07e7263bbf1bbb30e2d6
Jesús Acevedo <jacevedo@toteat.com>
2025-05-12 12:27:21 -0400

    feat: remove base path configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokedex/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
